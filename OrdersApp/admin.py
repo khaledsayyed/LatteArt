00fe4fba4172fdfd48e3af15b4d888a15a9b9ce9
rawane96 <rke15@mail.aub.edu>
2018-03-24 11:37:54 +0200

    Update admin.py

--- OrdersApp/admin.py
+++ OrdersApp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import CustomerProfile
 from .models import Product
 from .models import Order
-admin.site.register(UserProfile)
+from .models import Branch
+from .models import Employee
+from .models import 
+admin.site.register(CustomerProfile)
 admin.site.register(Product)
 admin.site.register(Order)
