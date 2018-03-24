ad07d0cefa7c7e4cebe62af4938969fc1f10cff4
rawane96 <rke15@mail.aub.edu>
2018-03-24 11:42:23 +0200

    Update tables.py

--- OrdersApp/tables.py
+++ OrdersApp/tables.py
@@ -1,5 +1,5 @@
 from .models import Order
-from .models import UserProfile
+from .models import CustomerProfile
 from .models import Product
 from table import Table
 from table.columns import Column
@@ -7,15 +7,15 @@ from table.columns import Column
 class UserTable(Table):
 	id = Column(field='id', header=u'Id')
 	Name = Column(field='Name', header=u'Name')
-	UserNamme = Column(field='UserName', header=u'UserName')
+	UserName = Column(field='UserName', header=u'UserName')
 	class Meta:
-		model = UserProfile
+		model = CustomerProfile
 	
 class OrderTable(Table):
 	id = Column(field='id', header=u'Id')
 	CustomerName = Column(field='Customer.Name', header=u'Customer')
-	ProductName = Column(field='Product.Name', header=u'Product')
-	ProductPrice = Column(field = 'Product.Price', header=u'Price')
+	#ProductName = Column(field='Products.Name', header=u'Product')
+	TotalPrice = Column(field = 'TotalPrice', header=u'Price')
 	PickupTime = Column(field = 'PickupTime',header=u'PickupTime')
 	PaymentMethod = Column(field = 'PaymentMethod',header=u'PaymentMethod')
 	class Meta:
