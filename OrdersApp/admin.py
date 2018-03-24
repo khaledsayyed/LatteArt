7f393cc81eb71d7d31214f13a48080b15cb6cc3c
rawane96 <rke15@mail.aub.edu>
2018-03-24 11:40:42 +0200

    Update admin.py

--- OrdersApp/admin.py
+++ OrdersApp/admin.py
@@ -4,7 +4,7 @@ from .models import Product
 from .models import Order
 from .models import Branch
 from .models import Employee
-from .models import 
+ 
 admin.site.register(CustomerProfile)
 admin.site.register(Product)
 admin.site.register(Order)
